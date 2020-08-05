2247c9367b29a104d2d01e599e2dffb569a56d74
Satyam Patel <sbp223@lehigh.edu>
2020-08-05 19:33:36 +0000

    began the Graph class, implemented the 2-dimensional array into the constructor

--- Graph.java
+++ Graph.java
@@ -1,3 +1,24 @@
 class Graph {
-  int[][] graph = 
+
+
+  Graph(int[][] graph) { 
+    
+  }
+
+  
+
+  public void add_edge(int from, int to) {
+    graph[from][to] = 1;
+  }
+
+  public void remove_edge(int from, int to) {
+    graph[from][to] = 0; 
+  }
+
+  public int[] traverse_bfs(int[] queue, int start) {
+    
+  
+    
+  }
+
 }
--- Main.java
+++ Main.java
@@ -1,7 +1,34 @@
+import java.util.Arrays;
+
 class Main {
   public static void main(String[] args) {
 
+    Stack s = new Stack(10); 
+    s.push(9);
+    s.push(8);
+    System.out.println(Arrays.toString(s.list));
 
+    Queue q = new Queue(10);
+    q.push(0);
+    q.push(1);
+    q.push(2);
+    q.push(3);
+    q.push(4);
+    q.push(5);
+    q.push(6);
+    q.push(7);
+    q.push(8);
+    q.push(9);
+
+    int[][] newG = new int[10][10];
+
+    Graph g = new Graph(newG);
+
+    
+
+
+
+    
  
 
 
